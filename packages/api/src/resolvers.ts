cae4766acb4503c2cd2d7dc5f9a5893cc66206d7
Michael Funk <funkstyr@gmail.com>
2018-07-19 11:37:10 -0700

    connect register

--- packages/api/src/resolvers.ts
+++ packages/api/src/resolvers.ts
@@ -1,12 +1,27 @@
+import * as bcrypt from "bcryptjs";
+
 import { ResolverMap } from "./types/graphql-utils";
+import { User } from "./entity/User";
 
 export const resolvers: ResolverMap = {
   Query: {
     hello: (_, { name }: GQL.IHelloOnQueryArguments) => `Bye ${name || "World"}`
   },
   Mutation: {
-    register: (_, { email, password }: GQL.IRegisterOnMutationArguments) => {
-      return email + password;
+    register: async (
+      _,
+      { email, password }: GQL.IRegisterOnMutationArguments
+    ) => {
+      const passwordHash = await bcrypt.hash(password, 15);
+
+      const user = User.create({
+        email,
+        password: passwordHash
+      });
+
+      await user.save();
+
+      return true;
     }
   }
 };
