1982a1250418abf70ec9c876fc5c834cc001520b
Michael Funk <funkstyr@gmail.com>
2018-07-28 16:26:45 -0700

    use description inputs for description step

--- packages/web/src/components/listing/create/components/DescriptionSet.tsx
+++ packages/web/src/components/listing/create/components/DescriptionSet.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Field } from "formik";
 import { InputField } from "../../../shared/InputField";
+import { DropzoneField } from "../../../shared/DropzoneField";
 
 // export class DescriptionSet extends React.PureComponent {
 //   render() {
@@ -46,36 +47,29 @@ import { InputField } from "../../../shared/InputField";
 export const DescriptionSet = () => (
   <React.Fragment>
     <Field
-      name="price"
-      placeholder="Price"
-      label="Price"
-      useNumberComponent={true}
-      type="input"
+      name="name"
+      placeholder="Name"
+      type="text"
+      component={InputField}
+      label="Name"
+      useNumberComponent={false}
+    />
+    <Field
+      name="category"
+      placeholder="Category"
+      label="Category"
+      useNumberComponent={false}
+      type="text"
       component={InputField}
     />
     <Field
-      name="beds"
-      placeholder="Beds"
-      label="Beds"
-      useNumberComponent={true}
-      type="input"
+      name="description"
+      placeholder="Description"
+      label="Description"
+      useNumberComponent={false}
+      type="text"
       component={InputField}
     />
-    <Field
-      name="baths"
-      placeholder="Baths"
-      label="Baths"
-      useNumberComponent={true}
-      type="input"
-      component={InputField}
-    />
-    <Field
-      name="guests"
-      placeholder="Guests"
-      label="Guests"
-      useNumberComponent={true}
-      type="input"
-      component={InputField}
-    />
+    <Field name="picture" component={DropzoneField} />
   </React.Fragment>
 );
