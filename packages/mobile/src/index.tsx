e73401402056630a31e59826537187373d75df93
Michael Funk <funkstyr@gmail.com>
2018-08-01 09:20:32 -0700

    add paths to tsconfig

--- packages/mobile/src/index.tsx
+++ packages/mobile/src/index.tsx
@@ -6,12 +6,13 @@ import { client } from "./apollo";
 import { RegisterConnector } from "./components/register/RegisterConnector";
 import { LoginConnector } from "./components/login/LoginConnector";
 import { Me } from "./components/user/Me";
+import { CreateListing } from "./components/listing/CreateListing";
 
 export default class App extends React.Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <Router initialEntries={["/login"]}>
+        <Router initialEntries={["/me"]}>
           <Switch>
             <Route
               exact={true}
@@ -20,6 +21,11 @@ export default class App extends React.Component {
             />
             <Route exact={true} path="/login" component={LoginConnector} />
             <Route exact={true} path="/me" component={Me} />
+            <Route
+              exact={true}
+              path="/create-listing"
+              component={CreateListing}
+            />
           </Switch>
         </Router>
       </ApolloProvider>
